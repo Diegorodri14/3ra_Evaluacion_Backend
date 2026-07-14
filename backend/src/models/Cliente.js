1fa98218b8fe7d3ad303ca09312e79f39f57544c
Diegorodri14 <diegorodrigueza14@gmail.com>
2026-07-14 07:36:05 -0600

    feat: Estructura de carpetas basica

---
+++ backend/src/models/Cliente.js
@@ -0,0 +1,14 @@
+/* 
+    name,
+    email,
+    password,
+    isVerified,
+    loginAttemps,
+    timeOut
+*/
+
+import {Schema, model} from 'mongoose';
+
+const clientesSchema = new Schema({
+
+})
